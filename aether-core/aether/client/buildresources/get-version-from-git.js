5559dda416d909477618a2534f28d0d1d90cff0f
“Frigyes06” <eszfrigyes06@gmail.com>
2023-06-04 12:04:51 -0700

    Fixed npm version error

--- aether-core/aether/client/buildresources/get-version-from-git.js
+++ aether-core/aether/client/buildresources/get-version-from-git.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync
 
 function main() {
   let version = (getBaseVersion() + '+' + getBaseBuildNumber()).toLowerCase()
-  execSync('npm version --allow-same-version ' + version)
+  execSync('npm version --allow-same-version --git-tag-version false ' + version)
   console.log(version)
 }
 
